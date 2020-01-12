09dbd07a760f5d510b8ebdce7b5a5aab14b4d6b6
beyonce knowles <beyonce@thinkful.com>
2020-01-12 10:31:07 -0600

    update

--- script.js
+++ script.js
@@ -10,8 +10,27 @@ function displayResults(responseJson) {
       console.log('stock not found')
       $('.js-error-message').html(`<p>Stock not found, please choose a valid Stock Symbol.</p>`)
   } else {
-    for (let i = 0; i < responseJson.ratios.length; i++) {
+
+    let date = [];
+    let currentRatio = [];
+    let quickRatio = [];
+    let inventoryTurnover = [];
+    let debtEquityRatio = [];
+    let returnOnEquity = [];
+    let returnOnAssets = [];
+
+
+    for (let i = 0; i < responseJson.ratios.length && i < 10; i++) {
       
+      date.push(`${responseJson.ratios[i].date}`);
+      currentRatio.push(`${responseJson.ratios[i].liquidityMeasurementRatios.currentRatio}`);
+      quickRatio.push(`${responseJson.ratios[i].liquidityMeasurementRatios.quickRatio}`);
+      inventoryTurnover.push(`${responseJson.ratios[i].operatingPerformanceRatios.inventoryTurnover}`);
+      debtEquityRatio.push(`${responseJson.ratios[i].debtRatios.debtEquityRatio}`);
+      returnOnEquity.push(`${responseJson.ratios[i].debtRatios.debtEquityRatio}`);
+      returnOnAssets.push(`${responseJson.ratios[i].profitabilityIndicatorRatios.returnOnAssets}`);
+
+
       //$('.showRatios').append(`
      // <p>Company name: ${responseJson.symbol}</p>
      // <p>Current Ratio: ${responseJson.ratios[i].liquidityMeasurementRatios.currentRatio}</p>
@@ -19,114 +38,59 @@ function displayResults(responseJson) {
      // `)
 
       
-      let myChart = document.getElementById('myChart').getContext('2d');
+     
+     
+
+        
+
+        
+  }
+
+   //Current Ratio Chart
+  let myChart = document.getElementById('myChart').getContext('2d');
+  
+      
+
 
         let ratioChart1 = new Chart(myChart,{
             type:'bar',
             data:{
-                labels:[`${responseJson.ratios[0].date}`,
-                        `${responseJson.ratios[1].date}`,
-                        `${responseJson.ratios[2].date}`,
-                        `${responseJson.ratios[3].date}`,
-                        `${responseJson.ratios[4].date}`,
-                        `${responseJson.ratios[5].date}`,
-                        `${responseJson.ratios[6].date}`,
-                        `${responseJson.ratios[7].date}`,
-                        `${responseJson.ratios[8].date}`,
-                        `${responseJson.ratios[9].date}`,
-                      ],
+                labels:date,
                 datasets:[{
                     label: 'Current Ratios',
-                    data:[
-                    `${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[1].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[2].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[3].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[4].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[5].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[6].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[7].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[8].liquidityMeasurementRatios.currentRatio}`,
-                    `${responseJson.ratios[9].liquidityMeasurementRatios.currentRatio}`,
-                   
-                    ],
+                    data:currentRatio,
                     backgroundColor:'green'
                 }]
             },
             options:{}
         });
 
-
+        //Quick Ratio Chart
         let myChart2 = document.getElementById('myChart2').getContext('2d');
 
         let ratioChart2 = new Chart(myChart2,{
             type:'bar',
             data:{
-                labels:[`${responseJson.ratios[0].date}`,
-                        `${responseJson.ratios[1].date}`,
-                        `${responseJson.ratios[2].date}`,
-                        `${responseJson.ratios[3].date}`,
-                        `${responseJson.ratios[4].date}`,
-                        `${responseJson.ratios[5].date}`,
-                        `${responseJson.ratios[6].date}`,
-                        `${responseJson.ratios[7].date}`,
-                        `${responseJson.ratios[8].date}`,
-                        `${responseJson.ratios[9].date}`,
-                      ],
+                labels:date,
                 datasets:[{
                     label: 'Quick Ratios',
-                    data:[
-                    `${responseJson.ratios[0].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[1].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[2].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[3].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[4].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[5].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[6].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[7].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[8].liquidityMeasurementRatios.quickRatio}`,
-                    `${responseJson.ratios[9].liquidityMeasurementRatios.quickRatio}`,
-
-                   
-                    ],
+                    data:quickRatio,
                     backgroundColor:'brown'
                 }]
             },
             options:{}
         });
 
-
+        //Invetory Turnover Ratio Chart
         let myChart3 = document.getElementById('myChart3').getContext('2d');
 
         let ratioChart3 = new Chart(myChart3,{
             type:'bar',
             data:{
-                labels:[`${responseJson.ratios[0].date}`,
-                        `${responseJson.ratios[1].date}`,
-                        `${responseJson.ratios[2].date}`,
-                        `${responseJson.ratios[3].date}`,
-                        `${responseJson.ratios[4].date}`,
-                        `${responseJson.ratios[5].date}`,
-                        `${responseJson.ratios[6].date}`,
-                        `${responseJson.ratios[7].date}`,
-                        `${responseJson.ratios[8].date}`,
-                        `${responseJson.ratios[9].date}`,
-                      ],
+                labels:date,
                 datasets:[{
                     label: 'Inventory Turnover Ratio',
-                    data:[
-                    `${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[1].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[2].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[3].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[4].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[5].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[6].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[7].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[8].operatingPerformanceRatios.inventoryTurnover}`,
-                    `${responseJson.ratios[9].operatingPerformanceRatios.inventoryTurnover}`,
-                   
-                    ],
+                    data:inventoryTurnover,
                     backgroundColor:'purple'
                 }]
             },
@@ -134,38 +98,16 @@ function displayResults(responseJson) {
         });
 
 
-
+        //Debt-Equity Ratio Chart
         let myChart4 = document.getElementById('myChart4').getContext('2d');
 
         let ratioChart4 = new Chart(myChart4,{
             type:'bar',
             data:{
-                labels:[`${responseJson.ratios[0].date}`,
-                        `${responseJson.ratios[1].date}`,
-                        `${responseJson.ratios[2].date}`,
-                        `${responseJson.ratios[3].date}`,
-                        `${responseJson.ratios[4].date}`,
-                        `${responseJson.ratios[5].date}`,
-                        `${responseJson.ratios[6].date}`,
-                        `${responseJson.ratios[7].date}`,
-                        `${responseJson.ratios[8].date}`,
-                        `${responseJson.ratios[9].date}`,
-                      ],
+                labels:date,
                 datasets:[{
                     label: 'Debt-Equity Ratio',
-                    data:[
-                    `${responseJson.ratios[0].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[1].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[2].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[3].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[4].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[5].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[6].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[7].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[8].debtRatios.debtEquityRatio}`,
-                    `${responseJson.ratios[9].debtRatios.debtEquityRatio}`,
-                   
-                    ],
+                    data:debtEquityRatio,
                     backgroundColor:'red'
                 }]
             },
@@ -173,38 +115,16 @@ function displayResults(responseJson) {
         });
 
 
-
+        //Return on Equity Chart
         let myChart5 = document.getElementById('myChart5').getContext('2d');
 
         let ratioChart5 = new Chart(myChart5,{
             type:'bar',
             data:{
-                labels:[`${responseJson.ratios[0].date}`,
-                        `${responseJson.ratios[1].date}`,
-                        `${responseJson.ratios[2].date}`,
-                        `${responseJson.ratios[3].date}`,
-                        `${responseJson.ratios[4].date}`,
-                        `${responseJson.ratios[5].date}`,
-                        `${responseJson.ratios[6].date}`,
-                        `${responseJson.ratios[7].date}`,
-                        `${responseJson.ratios[8].date}`,
-                        `${responseJson.ratios[9].date}`,
-                      ],
+                labels:date,
                 datasets:[{
                     label: 'return on equity',
-                    data:[
-                    `${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[1].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[2].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[3].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[4].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[5].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[6].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[7].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[8].profitabilityIndicatorRatios.returnOnEquity}`,
-                    `${responseJson.ratios[9].profitabilityIndicatorRatios.returnOnEquity}`,
-                   
-                    ],
+                    data:returnOnEquity,
                     backgroundColor:'fuschia'
                 }]
             },
@@ -215,55 +135,91 @@ function displayResults(responseJson) {
 
 
 
-
+        //Return on Assets Chart
         let myChart6 = document.getElementById('myChart6').getContext('2d');
 
         let ratioChart6 = new Chart(myChart6,{
             type:'bar',
             data:{
-                labels:[`${responseJson.ratios[0].date}`,
-                        `${responseJson.ratios[1].date}`,
-                        `${responseJson.ratios[2].date}`,
-                        `${responseJson.ratios[3].date}`,
-                        `${responseJson.ratios[4].date}`,
-                        `${responseJson.ratios[5].date}`,
-                        `${responseJson.ratios[6].date}`,
-                        `${responseJson.ratios[7].date}`,
-                        `${responseJson.ratios[8].date}`,
-                        `${responseJson.ratios[9].date}`,
-                      ],
+                labels:date,
                 datasets:[{
                     label: 'Return on Assets',
-                    data:[
-                    `${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[1].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[2].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[3].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[4].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[5].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[6].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[7].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[8].profitabilityIndicatorRatios.returnOnAssets}`,
-                    `${responseJson.ratios[9].profitabilityIndicatorRatios.returnOnAssets}`,
-                   
-                    ],
+                    data:returnOnAssets,
                     backgroundColor:'purple'
                 }]
             },
             options:{}
         });
 
-        
-
-        
-  }
-  
+  //Shows Ratio Info on DOM
   $('.info').show(400);
   
-    if(`${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` < 2 ){
-      console.log('hello world')
+  //Calculates grade for Current Ratio in latest year
+    if(`${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` > 1.4 && `${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` < 1.6 ){
+      console.log('A')
+    } else if(`${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` > 1.7 && `${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` < 3 ) {
+      console.log('B')
+    } else if(`${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` > 1 && `${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` < 1.3 ){
+      console.log('C')
+    } else if(`${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` > 3) {
+      console.log('D')
+    } else if (`${responseJson.ratios[0].liquidityMeasurementRatios.currentRatio}` < 1 ){
+      console.log('F')
     }
-     
+
+    //Calculates grade for Quick ratio in latest year
+     if(`${responseJson.ratios[0].liquidityMeasurementRatios.quickRatio}` >= 1){
+      console.log('A')
+    } else if (`${responseJson.ratios[0].liquidityMeasurementRatios.quickRatio}` < 1) {
+      console.log('F')
+    }
+
+   //Calculates grade for Inventory Turnover ratio
+   
+   if(`${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}` > 4 && `${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}` < 6) {
+    console.log('A')
+   } else if (`${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}` > 6 && `${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}` < 500) {
+     console.log('B')
+   } else if (`${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}` > 2 && `${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}`< 3 ) {
+     console.log('C')
+   } else if (`${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}` > 1 && `${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}`< 1.9) {
+     console.log('D')
+   } else if(`${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}` < 1 || `${responseJson.ratios[0].operatingPerformanceRatios.inventoryTurnover}` > 500) {
+     console.log('F')
+   }
+
+
+   //Calculates grade for debt equity ratio
+
+   if(`${responseJson.ratios[0].debtRatios.debtEquityRatio}` > 1.5 && `${responseJson.ratios[0].debtRatios.debtEquityRatio}` < 2){
+    console.log('A')
+   } else if(`${responseJson.ratios[0].debtRatios.debtEquityRatio}` > 1 && `${responseJson.ratios[0].debtRatios.debtEquityRatio}` < 1.4) {
+     console.log('B')
+   } else if(`${responseJson.ratios[0].debtRatios.debtEquityRatio}` > 2 && `${responseJson.ratios[0].debtRatios.debtEquityRatio}` < 4) {
+     console.log('C')
+   } else if(`${responseJson.ratios[0].debtRatios.debtEquityRatio}` > 4 && `${responseJson.ratios[0].debtRatios.debtEquityRatio}` < 7) {
+     console.log('D')
+   } else if(`${responseJson.ratios[0].debtRatios.debtEquityRatio}` < 1 || `${responseJson.ratios[0].debtRatios.debtEquityRatio}` > 7) {
+     console.log('F')
+   }
+
+   //Calculates grade for return on equity ratio
+   if(`${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}` > .15){
+    console.log('A')
+   } else if(`${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}` > .10 && `${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}` < .14) {
+     console.log('B')
+   } else if (`${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}` > .05 && `${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}` < .10) {
+     console.log('C');
+   } else if(`${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}` > .01 && `${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}` < .05) {
+     console.log('D')
+   } else if(`${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnEquity}`< .01) {
+     console.log('F')
+   }
+
+   //Calculates grade for return on asset ratio
+
+   // if(`${responseJson.ratios[0].profitabilityIndicatorRatios.returnOnAssets}`)
+
   }
 
  
